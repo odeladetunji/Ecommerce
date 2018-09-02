7b986977ffbc65797344a8038f4c952f714ab71d
odeladetunji <odeladetunji@gmail.com>
2018-09-02 01:00:49 +0100

    committing

--- public/js/landingpage.js
+++ public/js/landingpage.js
@@ -6,23 +6,75 @@ function LandingPage(props){
 	      });
           
           var counter = 0;
-          
-          for(var i=0; i<=products.length; i++){
-              if (counter == 4) {
+ 
+          // for bigger screen;
+          () => {
+		          	 for(var i=0; i<=products.length; i++){
+		              if (counter > 0 && counter < 5) {
 
-              }
+		              }
 
-              if (counter == 3) {
+		              if (counter > 4 && counter < 9) {
 
-              }
+		              }
 
-              if (counter == 2){
+		              if (counter > 8 && counter < 13){
 
-              }
+		              }
 
-              if (counter == 1) {
+		              if (counter > 12 && counter < 17) {
 
-              }
+		              }
+		          }
+          }
+           
+          // for middium screen!
+          () => {
+          	         for(var i=0; i<=products.length; i++){
+			              if (counter > 0 && counter < 4) {
+
+			              }
+
+			              if (counter > 3 && counter < 7) {
+
+			              }
+
+			              if (counter > 6 && counter < 10){
+
+			              }
+
+			              if (counter > 9 && counter < 13) {
+
+			              }
+			          }
+          }
+
+          // for smaller screen!
+         () => {
+          	         for(var i=0; i<=products.length; i++){
+			              if (counter > 0 && counter < 3) {
+
+			              }
+
+			              if (counter > 2 && counter < 5) {
+
+			              }
+
+			              if (counter > 4 && counter < 7){
+
+			              }
+
+			              if (counter > 6 && counter < 9) {
+
+			              }
+			          }
+          }
+
+          //for small screen!
+          () => {
+          	         for(var i=0; i<=products.length; i++){
+			              // just insert on object per role!
+			          }
           }
 	}
     
@@ -32,24 +84,6 @@ function LandingPage(props){
           });
           
           var counter = 0;
-
-          for(var i=0; i<=blogs.length; i++){
-          	  if (counter == 4) {
-
-              }
-
-              if (counter == 3) {
-
-              }
-
-              if (counter == 2){
-
-              }
-
-              if (counter == 1) {
-              	
-              }
-          }
     }
 
 	return <div>
